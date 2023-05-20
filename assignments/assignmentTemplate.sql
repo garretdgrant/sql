b2df171c2b22debefed1122171cd36ef8e411ffc
Garret G <g.grant92@gmail.com>
2023-05-20 07:20:12 -0700

    added assignment template

---
+++ assignments/assignmentTemplate.sql
@@ -0,0 +1,47 @@
+/*  Assignment7.sql 
+    Garret Grant
+    CS 31A, Spring 2023 */
+--
+-- Current Database: `megastore`
+--
+
+USE megastore;
+
+/* Query 1*/
+
+/* Query 2 */
+
+
+/* Query 3 */
+
+
+/* Query 4 */
+
+
+/* Query 5 */
+
+
+/* Query 6 */
+
+
+/* Query 7 */
+
+
+/* Query 8 */
+
+
+/* Query 9 */
+
+
+/*  Query 10 */
+
+/* Query 11 */
+
+
+/*  Query 12 */
+
+
+/*  Query 13 */
+
+
+/*  Query 14 */
